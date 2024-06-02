8451a52c89a4291a1739336c3f2c87ea77fc7ee3
Adrián Melián <adrianmgdev@gmail.com>
2024-06-02 20:54:20 +0100

    Updated Task component to retrive data from API and show it.

--- src/app/components/Tasks/task-list/task-list.component.ts
+++ src/app/components/Tasks/task-list/task-list.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject, skip, takeUntil } from 'rxjs';
+import { Task, TaskStatus } from '@Models/tasks.model';
+import { TasksService } from '@Services/tasks.service';
 @Component({
   selector: 'app-task-list',
   standalone: true,
   imports: [],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrl: './task-list.component.scss',
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit, OnDestroy {
+  private _unsubscribeAll: Subject<any>;
 
+  statusList: Array<TaskStatus> = [];
+  taksList: Array<Task> = [];
+
+  constructor(private _taskService: TasksService) {
+    this._unsubscribeAll = new Subject();
+  }
+
+  ngOnInit(): void {
+    this._taskService.onTasksConstantsGet
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe((constants: { statusList: Array<TaskStatus> }) => {
+        this.statusList = constants.statusList;
+        console.info(this.statusList);
+      });
+
+    this._taskService.onTasksLoad
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe((tasks: Array<Task>) => {
+        this.taksList = tasks;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribeAll.next(undefined);
+    this._unsubscribeAll.complete();
+  }
 }
