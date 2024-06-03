593240849e99b1ef1102d70f010adc514594d900
Adrián Melián <adrianmgdev@gmail.com>
2024-06-03 14:40:30 +0100

    TaskModel - Properties update (task dates)

--- src/app/models/tasks.model.ts
+++ src/app/models/tasks.model.ts
@@ -1,28 +1,31 @@
 export interface ITask {
-  id: number;
-  guid: string;
+  id?: string;
   title: string;
   description: string;
   status: number;
+  startDate: string;
+  dueDate: string;
   author: string;
 }
 
 export class Task {
-  id: number | undefined;
-  guid: string | undefined;
-  title: string | undefined;
-  description: string | undefined;
-  statusId: number | undefined;
+  id: string;
+  title: string;
+  description: string;
+  statusId: number;
   statusName: string | undefined;
-  author: string | undefined;
+  author: string;
+  startDate: string;
+  dueDate: string;
 
   constructor(task: any, types?: Array<TaskStatus>) {
     this.id = task?.id ?? undefined;
-    this.guid = task?.guid ?? undefined;
     this.title = task?.title ?? undefined;
     this.description = task?.description ?? undefined;
     this.statusId = task?.status ?? undefined;
     this.author = task?.author ?? undefined;
+    this.startDate = task?.startDate ?? undefined;
+    this.dueDate = task?.dueDate ?? undefined;
 
     if (types) {
       this.SetStatusName(types);
