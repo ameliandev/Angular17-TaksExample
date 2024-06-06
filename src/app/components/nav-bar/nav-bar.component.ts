536e7413f321fa10e5321330d864b553700fbef5
Adrián Melián <adrianmgdev@gmail.com>
2024-06-06 03:02:37 +0100

    NavBar Component main functionality

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { UserData } from '@Modules/Auth/models/user-data.model';
+import { UserDataService } from '@Modules/Auth/services/user-data.service';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  visible = false;
+  _userData: UserData | undefined = undefined;
 
+  private _userDataService: UserDataService = inject(UserDataService);
+
+  ngOnInit(): void {
+    this._userData = this._userDataService.Data;
+    this.visible = this._userData !== undefined;
+  }
 }
