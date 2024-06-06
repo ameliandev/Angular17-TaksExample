8274a824c7d0c62ed6a1777598522f50cd1b2888
Adrián Melián <adrianmgdev@gmail.com>
2024-06-06 01:54:56 +0100

    LoginComponent - Version 1

--- src/app/modules/Auth/components/login/login.component.ts
+++ src/app/modules/Auth/components/login/login.component.ts
@@ -1,56 +1,110 @@
+import { CommonModule } from '@angular/common';
 import { UserAuthService } from '@Modules/Auth/services/user-auth.service';
 import { Component, OnInit, inject } from '@angular/core';
 import { UserDataService } from '../../services/user-data.service';
 import { Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+
+import {
+  FormsModule,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  isLoading = false;
+
   constructor(
     private authService: UserAuthService,
     private userService: UserDataService,
-    private router: Router
-  ) {}
-  ngOnInit(): void {
-    this.Login();
+    private router: Router,
+    private _formBuilder: FormBuilder
+  ) {
+    this.form = new FormGroup({});
   }
 
-  async Login() {
-    if (this.userService.Data !== undefined) {
+  ngOnInit(): void {
+    this.setForm();
+  }
+
+  async onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+
+    try {
+      if (this.userService.Data !== undefined) {
+        this.router.navigate(['tasks']);
+        return;
+      }
+
+      const email = this.form.get('email')?.value; // 'admin@taskman.com';
+      const pass = this.form.get('password')?.value; //'sL)bA=.RU!t62D$Hz`SP9w';
+
+      // const email = 'adrianmelian@taskman.com';
+      // const pass = 'Ma-9E+p3TB=qQXLR{wjW;Y';
+
+      const result = await this.authService.Login(email, pass);
+
+      if (!result) {
+        return;
+      }
+
+      const data = await this.userService.read(email);
+
+      if (!data) {
+        return;
+      }
+
+      this.userService.Data = data.pop();
+      await this.userService.read();
+      this.isLoading = false;
+
       this.router.navigate(['tasks']);
-      return;
+    } catch (error) {
+      this.isLoading = false;
     }
+  }
 
-    const email = 'admin@taskman.com';
-    const pass = 'sL)bA=.RU!t62D$Hz`SP9w';
-
-    // const email = 'adrianmelian@taskman.com';
-    // const pass = 'Ma-9E+p3TB=qQXLR{wjW;Y';
-
-    const result = await this.authService.Login(email, pass);
-
-    if (!result) {
-      return;
-    }
-
-    const data = await this.userService.read(email);
-
-    if (!data) {
-      return;
-    }
-
-    this.userService.Data = data.pop();
-
-    await this.userService.read();
-
-    console.info('USERS', this.userService.Users);
-
-    this.router.navigate(['tasks']);
+  /**
+   * Set the FormGroup properties
+   */
+  setForm() {
+    this.form = this._formBuilder.group({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
   }
 }
