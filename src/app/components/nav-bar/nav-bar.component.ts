6679cc7d4b805e31992cdca6976322a03c136a56
Adrián Melián <adrianmgdev@gmail.com>
2024-06-06 10:28:18 +0100

    Navbar - Complete

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,22 +1,40 @@
 import { UserData } from '@Modules/Auth/models/user-data.model';
+import { UserAuthService } from '@Modules/Auth/services/user-auth.service';
 import { UserDataService } from '@Modules/Auth/services/user-data.service';
-import { Component, OnInit, inject } from '@angular/core';
-
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit, OnDestroy {
   visible = false;
   _userData: UserData | undefined = undefined;
 
   private _userDataService: UserDataService = inject(UserDataService);
+  private _userAuthService: UserAuthService = inject(UserAuthService);
+  private _router: Router = inject(Router);
 
   ngOnInit(): void {
-    this._userData = this._userDataService.Data;
-    this.visible = this._userData !== undefined;
+    this._userDataService.onUserDataSet.subscribe((userData) => {
+      this._userData = userData;
+      this.visible = this._userData !== undefined;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.visible = false;
+  }
+
+  onLogout() {
+    const result = this._userAuthService.Logout();
+    if (!result) {
+      return;
+    }
+    this._router.navigate(['login']);
   }
 }
